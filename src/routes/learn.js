7af89e76a7501863f755e0999fdab449b09e087b
Airing <gx-deng@163.com>
2018-04-06 21:20:41 +0800

    feat(learn): 增加出题接口

--- src/routes/learn.js
+++ src/routes/learn.js
@@ -14,7 +14,7 @@ const router = express.Router()
 
 // 随机出题接口
 // 前端请求用于获取20条数据作为一组题目
-router.get('/get_learn', (req, res) => {
+router.get('/get_learns', (req, res) => {
 
   const {token, uid, timestamp} = req.query
   validate(res, true, uid, timestamp, token)
@@ -26,6 +26,35 @@ router.get('/get_learn', (req, res) => {
     }
     const data = await Resource.findAll({where: {id: ids}})
     return res.json({...MESSAGE.OK, data})
+  }
+  response()
+})
+
+// 随机出题接口
+// 前端请求用于获取1道题目
+router.get('/get_learn', (req, res) => {
+
+  const {token, uid, timestamp} = req.query
+  validate(res, true, uid, timestamp, token)
+
+  const shuffle = (arr) => {
+    for (let i = arr.length - 1; i >= 0; i--) {
+      const randomIndex = Math.floor(Math.random() * (i + 1))
+      const itemAtIndex = arr[randomIndex]
+      arr[randomIndex] = arr[i]
+      arr[i] = itemAtIndex
+    }
+    return arr
+  }
+
+  const response = async () => {
+    const data = await Resource.findOne({where: {id: Math.floor(Math.random() * (1326 - 63 + 1) + 63)}})
+    const resources = await Resource.findAll({where: {id: [Math.floor(Math.random() * (1326 - 63 + 1) + 63), Math.floor(Math.random() * (1326 - 63 + 1) + 63), Math.floor(Math.random() * (1326 - 63 + 1) + 63)]}})
+    let resources_name = await resources.map(r => r.name)
+    resources_name[3] = data.name
+    
+    const options = shuffle(resources_name)
+    return res.json({...MESSAGE.OK, data, options})
   }
   response()
 })
